bca8f7f81576a39695e803504bd71a25609ea245
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-06-26 11:59:54 -0300

    4.1. Small refactor to models

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 2020;
-const User = require('./models/User');
-const Recipe = require('./models/Recipe');
+const { User, Recipe } = require('./models');
 
 require('dotenv').config({ path: 'vars.env' });
 
@@ -14,4 +13,5 @@ mongoose
 
 app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`);
+  console.log(User, Recipe);
 });
