c34d59677870eb23f0c2fa48619eefdaf9d93738
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-06-26 10:29:08 -0300

    2. Added mongoose and dotenv settings

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 2020;
 
+require('dotenv').config({ path: 'vars.env' });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Database connection stablished'))
+  .catch(console.error);
+
 app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`);
+  console.log(process.env.MONGO_URI);
 });
