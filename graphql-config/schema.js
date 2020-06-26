f5f7197b67b5b2f0c17d1fb20032f06b1a0e2328
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-06-26 17:29:26 -0300

    5. Adds the basic graphql config for our app: Schemas and resolvers

---
+++ graphql-config/schema.js
@@ -0,0 +1,21 @@
+exports.typeDefs = `
+  type User {
+    username: String!
+    password: String!
+    email: String!
+    joinDate: String
+    favorites: [Recipe]
+  }
+  type Recipe {
+    name: String!
+    category: String!
+    description: String!
+    instructions: String!
+    createDate: String
+    likes: Int
+    username: String!
+  }
+  type Query {
+    getAllRecipes: [Recipe]
+  }
+`;
